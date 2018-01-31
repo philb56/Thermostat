cedf657d8b7a2c55db75f2594bd04d0974e8bd90
Lewis Moore <lewis.moore2707@gmail.com>
2018-01-31 17:36:43 +0000

    Implement increase temperature in jquery with working view

--- interface.js
+++ interface.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
+
+  $('#increase-temperature').on('click', function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature);
+  })
 })
