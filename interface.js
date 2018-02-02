22dea668f8a50bc0169d8a003cc2bb3a775abb24
philb56 <phil.bugden@outlook.com>
2018-02-02 14:21:01 +0000

    End of CSS for now

--- interface.js
+++ interface.js
@@ -28,17 +28,31 @@ $(document).ready(function() {
     thermostat.turnPowerSaveOff();
     $('#power-saving-status').text('Off');
   })
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.currentUsage());
   }
+
   displayWeather('London');
 
   $('#current-city').change(function(event) {
     event.preventDefault();
     var city = $('#current-city').val();
     displayWeather(city);
+    displayImage(city);
   })
+
+  function displayImage(city){
+    var cityHash = {};
+    cityHash["newyork"] = "http://www.karatetoc.com/Statue-of-liberty-NYC-Black-and-white-photography-12.jpg";
+    cityHash["london"] = "https://www.mrsmithworldphotography.com/photos/Houses-of-Parliament-11.jpg";
+    cityHash["tokyo"] = "https://i.pinimg.com/originals/c8/40/ea/c840ea63792d5a68a58b3be5907ea13f.jpg";
+    cityHash["paris"] = "http://data.whicdn.com/images/27757411/awesome-beautiful-beauty-black-and-white-i-miss-you-Favim.com-413264_large.jpg";
+    console.log(city);
+    console.log(cityHash);
+    $('body').css('background-image', 'url(' + cityHash[city] + ')');
+  }
 
   function displayWeather(city) {
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
